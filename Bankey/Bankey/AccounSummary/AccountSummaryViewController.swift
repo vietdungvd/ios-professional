4cc031eaa8ce6c3c2e03b7a890b0efb4c416ddf2
DungTV <dungtv@moneylover.me>
2022-06-07 11:04:18 +0700

    feat: Group account summary network calls together

--- Bankey/Bankey/AccounSummary/AccountSummaryViewController.swift
+++ Bankey/Bankey/AccounSummary/AccountSummaryViewController.swift
@@ -104,27 +104,34 @@ extension AccountSummaryViewController {
 // MARK: - Networking
 extension AccountSummaryViewController {
     private func fetchDataAndLoadViews() {
+        let group = DispatchGroup()
         
+        group.enter()
         fetchProfile(forUserId: "1") { result in
             switch result {
             case .success(let profile):
                 self.profile = profile
                 self.configureTableHeaderView(with: profile)
-                self.tableView.reloadData()
             case .failure(let error):
                 print(error.localizedDescription)
             }
+            group.leave()
         }
 
+        group.enter()
         fetchAccounts(forUserId: "1") { result in
             switch result {
             case .success(let accounts):
                 self.accounts = accounts
                 self.configureTableCells(with: accounts)
-                self.tableView.reloadData()
             case .failure(let error):
                 print(error.localizedDescription)
             }
+            group.leave()
+        }
+        
+        group.notify(queue: .main) {
+            self.tableView.reloadData()
         }
     }
     
